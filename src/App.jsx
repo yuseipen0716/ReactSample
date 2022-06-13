9e2d5da03650939153f32c8ffbf53027ca6f6089
yuseipen0716 <yuseipen1@gmail.com>
2022-06-14 01:06:06 +0900

    名前入力フォーム作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 import NameArea from './components/NameArea';
 
-// const style = {
-//   position: "absolute",
-//   top: "87.4%",
-//   left: "59%",
-//   fontSize: "1rem"
-// };
 
-// function App(props) {
-//   return (
-//     <div style={style}>
-//       <input type="text" style={{border: "none", width: "10%"}} value={props.value} onChange={handleChange}></input>
-//       <button onClick={onClickButton}>OK</button>
-//     </div>
-//   );
-// }
 const App = () => {
   const [name, setName] = useState("");
-  const [border, setBorder] = useState(false);
 
   const onChangeName = (event) => setName(event.target.value);
-  const onClickInputArea = () => setBorder(!border)
 
   return (
     <>
       <NameArea
         name={name}
         onChange={onChangeName}
-        border={border}
-        onClick={onClickInputArea}
       />
     </>
   )
--- src/components/NameArea.jsx
+++ src/components/NameArea.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
+const nameLocate = {
+  position: "absolute",
+  top: "12%",
+  left: "9%",
+}
+
 const NameArea = (props) => {
+  const {name, onChange} = props;
+  const inputName = name != '' ? {
+    fontSize: "2em",
+    border: "none",
+  } : {
+    fontSize: "2em",
+  };
   return(
-    <div>
-      { props.border ? (
-        <input
-        placeholder="氏名を入力"
-        value={props.name}
-        onChange={props.onChange}
-        onClick={props.onClick}
-        style={{border: "none"}}
-        // やっぱボタン押してborder noneにする方がわかりやすいか
+    <div style={nameLocate}>
+      <input
+        placeholder='氏名を入力'
+        value={name}
+        onChange={onChange}
+        style={inputName}
       />
-      ) : (
-        <input
-        placeholder="氏名を入力"
-        value={props.name}
-        onChange={props.onChange}
-        onClick={props.onClick}
-      />
-      ) }
     </div>
   )
 };
