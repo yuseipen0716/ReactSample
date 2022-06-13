53d9647ca9596be9508fed4c79f88cb0feb0eb34
yuseipen0716 <yuseipen1@gmail.com>
2022-06-14 02:33:11 +0900

    日付位置合わせ

--- src/components/NameArea.jsx
+++ src/components/NameArea.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const nameLocate = {
+const dateLocate = {
   position: "absolute",
-  top: "12%",
-  left: "9%",
+  top: "6.5%",
+  left: "22.8%"
 }
 const furiganaLocate = {
   position: "absolute",
   top: "8.6%",
   left: "10%",
+}
+const nameLocate = {
+  position: "absolute",
+  top: "12%",
+  left: "9%",
 }
 
 const NameArea = (props) => {
@@ -28,6 +33,12 @@ const NameArea = (props) => {
 
   return(
     <>
+      <div style={dateLocate}>
+        <input value='2022' style={{width: "6%", marginRight: "1.5rem"}} />
+        <input value='02' style={{width: "3%", marginRight: "1.3rem"}} />
+        <input value='22' style={{width: "3%", marginRight: "1.3rem"}} />
+      </div>
+      {/* 明日はここの続きから。ここのstate管理というか、リファクタリングを。 */}
       <div style={furiganaLocate}>
       <input
         placeholder='フリガナを入力'
