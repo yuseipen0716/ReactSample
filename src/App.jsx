2ec86fbb207dfc766463b4b5b44744ee0eeeb536
yuseipen0716 <yuseipen1@gmail.com>
2022-06-14 19:13:59 +0900

    生年月日、性別欄を実装

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,32 @@ import { Image } from './components/Image';
 
 
 const App = () => {
+  // NameArea
   const [name, setName] = useState("");
   const [furigana, setFurigana] = useState("");
-
+  const [todayYear, setTodayYear] = useState("");
+  const [todayMonth, setTodayMonth] = useState("");
+  const [todayDate, setTodayDate] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [birthMonth, setBirthMonth] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [age, setAge] = useState("");
+  const [check, setCheck] = useState(0);
   const onChangeName = (event) => setName(event.target.value);
   const onChangeFurigana = (event) => setFurigana(event.target.value);
+  const onChangeTodayYear = (event) => setTodayYear(event.target.value);
+  const onChangeTodayMonth = (event) => setTodayMonth(event.target.value);
+  const onChangeTodayDate = (event) => setTodayDate(event.target.value);
+  const onChangeBirthYear = (event) => setBirthYear(event.target.value);
+  const onChangeBirthMonth = (event) => setBirthMonth(event.target.value);
+  const onChangeBirthDate = (event) => setBirthDate(event.target.value);
+  const onChangeAge = (event) => setAge(event.target.value);
+  const onClickCheckFirst = () => {
+    check === 0 ? setCheck(1) : setCheck(0)
+  }
+  const onClickCheckSecond = () => {
+    check === 0 ? setCheck(2) : setCheck(0)
+  }
 
   return (
     <>
@@ -19,6 +40,23 @@ const App = () => {
         onChangeName={onChangeName}
         furigana={furigana}
         onChangeFurigana={onChangeFurigana}
+        todayYear={todayYear}
+        onChangeTodayYear={onChangeTodayYear}
+        todayMonth={todayMonth}
+        onChangeTodayMonth={onChangeTodayMonth}
+        todayDate={todayDate}
+        onChangeTodayDate={onChangeTodayDate}
+        birthYear={birthYear}
+        onChangeBirthYear={onChangeBirthYear}
+        birthMonth={birthMonth}
+        onChangeBirthMonth={onChangeBirthMonth}
+        birthDate={birthDate}
+        onChangeBirthDate={onChangeBirthDate}
+        age={age}
+        onChangeAge={onChangeAge}
+        check={check}
+        onClickCheckFirst={onClickCheckFirst}
+        onClickCheckSecond={onClickCheckSecond}
       />
     </>
   )
--- src/components/NameArea.jsx
+++ src/components/NameArea.jsx
@@ -2,43 +2,133 @@ import React from 'react'
 
 const dateLocate = {
   position: "absolute",
-  top: "6.5%",
-  left: "22.8%"
+  top: "6.4rem",
+  left: "30rem"
 }
 const furiganaLocate = {
   position: "absolute",
-  top: "8.6%",
-  left: "10%",
+  top: "8.5rem",
+  left: "13rem",
 }
 const nameLocate = {
   position: "absolute",
-  top: "12%",
-  left: "9%",
+  top: "12rem",
+  left: "12rem",
+}
+const birthdayLocate = {
+  position: "absolute",
+  top: "17.2rem",
+  left: "9.5rem",
+}
+const checkLocate ={
+  position: "absolute",
+  top: "17rem",
+  left: "35.6rem",
 }
 
 const NameArea = (props) => {
-  const {name, onChangeName, furigana, onChangeFurigana} = props;
+  const {todayYear, onChangeTodayYear, todayMonth, onChangeTodayMonth, todayDate, onChangeTodayDate, name, onChangeName, furigana, onChangeFurigana,
+    birthYear, onChangeBirthYear, birthMonth, onChangeBirthMonth, birthDate, onChangeBirthDate, age, onChangeAge, check, onClickCheckFirst, onClickCheckSecond} = props;
   const inputName = name !== '' ? {
-    fontSize: "2rem",
+    fontSize: "2.5rem",
     border: "none",
   } : {
-    fontSize: "2rem",
+    fontSize: "2.5rem",
   };
-  const inputFurigana = furigana != '' ? {
+  const inputFurigana = furigana !== '' ? {
     fontSize: "1rem",
     border: "none",
   } : {
     fontSize: "1rem",
   };
+  const inputTodayYear = todayYear !== '' ? {
+    width: "3rem",
+    border: "none",
+    marginRight: "1.5rem",
+  } : {
+    width: "3rem",
+    marginRight: "1.5rem",
+  }
+  const inputTodayMonth = todayMonth !== '' ? {
+    width: "1.5rem",
+    border: "none",
+    marginRight: "1.5rem",
+  } : {
+    width: "1.5rem",
+    marginRight: "1.5rem",
+  }
+  const inputTodayDate = todayDate !== '' ? {
+    width: "1.5rem",
+    border: "none",
+    marginRight: "1.5rem",
+  } : {
+    width: "1.5rem",
+    marginRight: "1.5rem",
+  }
+  const inputBirthYear = birthYear !== '' ? {
+    width: "3rem",
+    border: "none",
+    marginRight: "2.5rem",
+  } : {
+    width: "3rem",
+    marginRight: "2.5rem",
+  }
+  const inputBirthMonth = birthMonth !== '' ? {
+    width: "1.5rem",
+    border: "none",
+    marginRight: "2.5rem",
+  } : {
+    width: "1.5rem",
+    marginRight: "2.5rem",
+  }
+  const inputBirthDate = birthDate !== '' ? {
+    width: "1.5rem",
+    border: "none",
+    marginRight: "2.5rem",
+  } : {
+    width: "1.5rem",
+    marginRight: "2.5rem",
+  }
+  const inputAge = age !== '' ? {
+    width: "1.5rem",
+    border: "none",
+    marginRight: "2.5rem",
+    marginLeft: "5.5rem",
+  } : {
+    width: "1.5rem",
+    marginRight: "2.5rem",
+    marginLeft: "5.5rem",
+  }
+  const checkbox = {
+    width: "2rem",
+    height: "1.8rem",
+    marginRight: "2.5rem",
+    border: "none",
+    backgroundColor: "rgba(255, 255, 255, .0)"
+  }
 
   return(
     <>
       <div style={dateLocate}>
-        <input value='2022' style={{width: "6%", marginRight: "1.5rem"}} />
-        <input value='02' style={{width: "3%", marginRight: "1.3rem"}} />
-        <input value='22' style={{width: "3%", marginRight: "1.3rem"}} />
+        <input
+          placeholder='2022'
+          value={todayYear}
+          onChange={onChangeTodayYear}
+          style={inputTodayYear} 
+        />
+        <input
+          placeholder='02'
+          value={todayMonth}
+          onChange={onChangeTodayMonth}
+          style={inputTodayMonth} 
+        />
+        <input
+          placeholder='02'
+          value={todayDate}
+          onChange={onChangeTodayDate}
+          style={inputTodayDate} 
+        />
       </div>
-      {/* 明日はここの続きから。ここのstate管理というか、リファクタリングを。 */}
       <div style={furiganaLocate}>
       <input
         placeholder='フリガナを入力'
@@ -54,6 +144,36 @@ const NameArea = (props) => {
           onChange={onChangeName}
           style={inputName}
         />
+      </div>
+      <div style={birthdayLocate}>
+        <input
+          placeholder='1994'
+          value={birthYear}
+          onChange={onChangeBirthYear}
+          style={inputBirthYear}
+        />
+        <input
+          placeholder='11'
+          value={birthMonth}
+          onChange={onChangeBirthMonth}
+          style={inputBirthMonth}
+        />
+        <input
+          placeholder='22'
+          value={birthDate}
+          onChange={onChangeBirthDate}
+          style={inputBirthDate}
+        />
+        <input
+          placeholder='27'
+          value={age}
+          onChange={onChangeAge}
+          style={inputAge}
+        />
+      </div>
+      <div style={checkLocate}>
+        <button style={checkbox} onClick={onClickCheckFirst}>{check === 0 || check === 2 ? "" : "✓"}</button>
+        <button style={checkbox} onClick={onClickCheckSecond}>{check === 0 || check === 1 ? "" : "✓"}</button>
       </div>
     </>
   )
