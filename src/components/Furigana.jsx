2c8d7b9d2505e2e6d3050a96b950afaf2a439e89
yuseipen0716 <yuseipen1@gmail.com>
2022-06-15 12:54:46 +0900

    フリガナ用のcss記述し、インラインstyleから移行

--- src/components/Furigana.jsx
+++ src/components/Furigana.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react'
 
 export const Furigana = (props) => {
-  // const {furigana, onChangeFurigana} = props;
   const [furigana, setFurigana] = useState("");
   const onChangeFurigana = (event) => setFurigana(event.target.value);
-
-  const inputFurigana = furigana !== '' ? {
-    fontSize: "1rem",
-    border: "none",
-  } : {
-    fontSize: "1rem",
-  };
 
   return (
     <input
       placeholder='フリガナを入力'
       value={furigana}
       onChange={onChangeFurigana}
-      style={inputFurigana}
+      className={furigana !== '' ? "furigana-border-none" : "furigana"}
     />
   )
 };
