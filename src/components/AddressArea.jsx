4b85b05a4e426e52a963ee3d86a20ede3f4e4773
yuseipen0716 <yuseipen1@gmail.com>
2022-06-15 14:14:44 +0900

    フリガナ欄(住所欄用)を新しいコンポーネントとして切り分け

---
+++ src/components/AddressArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+import { FuriganaLong } from './FuriganaLong';
+
+export const AddressArea = () => {
+  return (
+    <>
+      <div className="furigana-locate-address-area-first">
+        <FuriganaLong />
+      </div>
+    </>
+  )
+};
---
+++ src/components/FuriganaLong.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export const FuriganaLong = (props) => {
+  const [furigana, setFurigana] = useState("");
+  const onChangeFurigana = (event) => setFurigana(event.target.value);
+
+  return (
+    <input
+      placeholder='フリガナを入力'
+      value={furigana}
+      onChange={onChangeFurigana}
+      className={furigana !== '' ? "furigana-long-border-none" : "furigana-long"}
+    />
+  )
+};
