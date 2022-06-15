d87441a3998e5ab22afa24f2aec402f448f14523
yuseipen0716 <yuseipen1@gmail.com>
2022-06-15 14:26:56 +0900

    郵便番号コンポーネント作成

--- src/components/AddressArea.jsx
+++ src/components/AddressArea.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../App.css';
 import { FuriganaLong } from './FuriganaLong';
+import { PostalNumber } from './PostalNumber';
 
 export const AddressArea = () => {
   return (
     <>
       <div className="furigana-locate-address-area-first">
         <FuriganaLong />
+      </div>
+      <div className="postal-number-first">
+        <PostalNumber />
       </div>
     </>
   )
