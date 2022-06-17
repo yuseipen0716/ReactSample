151bf4a082c641784586549c598ba3009b4c229a
yuseipen0716 <yuseipen1@gmail.com>
2022-06-17 15:18:00 +0900

    左下の経歴欄を全て入力可能に

--- src/components/HistoryArea.jsx
+++ src/components/HistoryArea.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { InputHistory } from './InputHistory';
 
 export const HistoryArea = () => {
-  const firstHistoryArea = [...Array(17)].map(x => "")
-  const secondHistoryArea = [...Array(5)].map(x => "")
-  const licenseArea = [...Array(9)].map(x => "")
+  const firstHistories = [...Array(17)].map(x => "")
+  const secondHistories = [...Array(5)].map(x => "")
+  const licenses = [...Array(9)].map(x => "")
 
   return (
     <>
       <div className="first-history-locate">
         <ul>
-          <InputHistory />
+          {
+            firstHistories.map((history, index) => {
+              return (
+                <InputHistory key={index} />
+              )
+            })
+          }
         </ul>
       </div>
     </>
