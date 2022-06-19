2ac088110076aeb4f426990e9c343fb64a3d00a6
yuseipen0716 <yuseipen1@gmail.com>
2022-06-19 17:20:01 +0900

    通勤時間等の欄を実装

---
+++ src/components/MoreInfoArea.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Check } from './Check';
+import { TwoDigit } from './TwoDigit';
+
+export const MoreInfoArea = () => {
+  return (
+    <>
+      <div className="hour-locate">
+        <TwoDigit />
+      </div>
+      <div className="minute-locate">
+        <TwoDigit />
+      </div>
+      <div className="family-menber-locate">
+        <TwoDigit />
+      </div>
+      <div className="partner-yes-locate">
+        <Check />
+      </div>
+      <div className="partner-no-locate">
+        <Check />
+      </div>
+      <div className="support-yes-locate">
+        <Check />
+      </div>
+      <div className="support-no-locate">
+        <Check />
+      </div>
+    </>
+  )
+};
