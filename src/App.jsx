62040ab908f9a224d7f1acd0fbe0844c24304e69
yuseipen0716 <yuseipen1@gmail.com>
2022-09-12 19:49:34 +0900

    一旦、画面のキャプチャをpdf化することはできた。べた書きで仮実装。TODO 必要な部分のみキャプチャできるように

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,45 @@ import { Image } from './components/Image';
 import { HistoryArea } from './components/HistoryArea';
 import { TextArea } from './components/TextArea';
 import { MoreInfoArea } from './components/MoreInfoArea';
+import { jsPDF } from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 
 const App = () => {
   // NameArea
   const [name, setName] = useState("");
   const onChangeName = (event) => setName(event.target.value);
+  const onClick = (event) => {
+    const element = document.getElementById('capture');
+    const doc = new jsPDF({
+      orientation: 'l',
+      format: 'a4',
+    });
+    html2canvas(element, {scale: 2}).then((canvas) => {
+      const dataURI = canvas.toDataURL("image/jpeg");
+      document.body.appendChild(canvas);
+      const width = doc.internal.pageSize.width;
+      doc.addImage(dataURI, 'JPEG', 0, 0, width, 0);
+      doc.save("resume.pdf");
+    });
+  };
 
   return (
     <>
-      <Image />
-      <NameArea
-        name={name}
-        onChangeName={onChangeName}
-      />
-      <AddressArea />
-      <HistoryArea />
-      <TextArea />
-      <MoreInfoArea />
+      <div id='capture'>
+        <Image />
+        <NameArea
+          name={name}
+          onChangeName={onChangeName}
+        />
+        <AddressArea />
+        <HistoryArea />
+        <TextArea />
+        <MoreInfoArea />
+      </div>
+      <div>
+        <button onClick={onClick}>PDFとして保存</button>
+      </div>
     </>
   )
 };
