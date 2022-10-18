17fe23f8ef12cd6606ae5e3c93109c961d2e680b
yuseipen0716 <yuseipen1@gmail.com>
2022-10-18 17:14:53 +0900

    storage使用にあたってconfigファイル等の追加、修正

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,6 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default db;
+export const db = getFirestore(app);
+export const storage = getStorage(app)
